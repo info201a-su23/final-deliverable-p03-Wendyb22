4bc786753fb942849043ba8dd6dbcbf0e0c5952d
Wendyb22 <wsb22@uw.edu>
2023-08-14 11:01:50 -0700

    updated app.R pages

--- vizgunviolence/app.R
+++ vizgunviolence/app.R
@@ -9,8 +9,14 @@
 
 library(shiny)
 
+# introduction page
+ui_intro <- fluidPage(
+  
+)
+
+
 # Define UI for application that draws a histogram
-ui <- fluidPage(
+ui_page1 <- fluidPage(
 
     # Application title
     titlePanel("Old Faithful Geyser Data"),
@@ -32,8 +38,71 @@ ui <- fluidPage(
     )
 )
 
+
+# page 2
+ui_page2 <- fluidPage(
+  
+  # Application title
+  titlePanel("Old Faithful Geyser Data"),
+  
+  # Sidebar with a slider input for number of bins 
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput("bins",
+                  "Number of bins:",
+                  min = 1,
+                  max = 50,
+                  value = 30)
+    ),
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+      plotOutput("distPlot")
+    )
+  )
+)
+
+
+# page3
+ui_page3 <- fluidPage(
+  
+  # Application title
+  titlePanel("Old Faithful Geyser Data"),
+  
+  # Sidebar with a slider input for number of bins 
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput("bins",
+                  "Number of bins:",
+                  min = 1,
+                  max = 50,
+                  value = 30)
+    ),
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+      plotOutput("distPlot")
+    )
+  )
+)
+
+
+# conclusion page
+ui_con <- fluidPage(
+  
+)
+
+
+
+# intropage server
+server_intro <- function(input, output) {
+  
+}
+
+
+
 # Define server logic required to draw a histogram
-server <- function(input, output) {
+server_1 <- function(input, output) {
 
     output$distPlot <- renderPlot({
         # generate bins based on input$bins from ui.R
@@ -46,6 +115,45 @@ server <- function(input, output) {
              main = 'Histogram of waiting times')
     })
 }
+
+
+# page 2 server
+server_2 <- function(input, output) {
+  
+  output$distPlot <- renderPlot({
+    # generate bins based on input$bins from ui.R
+    x    <- faithful[, 2]
+    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    
+    # draw the histogram with the specified number of bins
+    hist(x, breaks = bins, col = 'darkgray', border = 'white',
+         xlab = 'Waiting time to next eruption (in mins)',
+         main = 'Histogram of waiting times')
+  })
+}
+
+# page 3 server
+server_3 <- function(input, output) {
+  
+  output$distPlot <- renderPlot({
+    # generate bins based on input$bins from ui.R
+    x    <- faithful[, 2]
+    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    
+    # draw the histogram with the specified number of bins
+    hist(x, breaks = bins, col = 'darkgray', border = 'white',
+         xlab = 'Waiting time to next eruption (in mins)',
+         main = 'Histogram of waiting times')
+  })
+}
+
+
+# conclusion page server
+server_con <- function(input, output) {
+  
+  
+}
+
 
 # Run the application 
 shinyApp(ui = ui, server = server)
